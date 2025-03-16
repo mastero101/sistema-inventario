07cf51d06f991599c9871230324f172c10d61219
mastero101 <mastershift_101@hotmail.com>
2025-03-16 03:27:59 -0600

    subidos environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sistema-inventario-back.vercel.app/api',
+  apiLoginUrl: 'https://demo-inventory-api.azurewebsites.net'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl2: 'http://localhost:3005/api',
+  apiUrl: 'https://sistema-inventario-back.vercel.app/api',
+  apiLoginUrl: 'https://demo-inventory-api.azurewebsites.net'
+};
