6785ed1e2fc919ea771e2f5a14aa9a8573a94501
mastero101 <mastershift_101@hotmail.com>
2025-03-27 21:54:57 -0600

    feat(inventory-paginated): add status styling and improve UI responsiveness
    Add `getStatusClass` method to apply dynamic styling based on inventory status. Enhance UI with responsive design, including mobile and desktop views, and improve pagination styling for better user experience.

--- src/app/components/inventory/inventory-paginated/inventory-paginated.component.ts
+++ src/app/components/inventory/inventory-paginated/inventory-paginated.component.ts
@@ -60,4 +60,19 @@ export class InventoryPaginatedComponent implements OnInit {
     this.currentPage = newPage;
     this.loadPaginatedInventory();
   }
+
+  getStatusClass(status: string): string {
+      switch (status) {
+        case 'Disponible':
+          return 'bg-green-100 text-green-800';
+        case 'Asignado':
+          return 'bg-blue-100 text-blue-800';
+        case 'Mantenimiento':
+          return 'bg-yellow-100 text-yellow-800';
+        case 'Dañado':
+          return 'bg-red-100 text-red-800';
+        default:
+          return 'bg-gray-100 text-gray-800';
+      }
+    }
 }
