f5b3071d288f31e909b644d36e5b04f581afde70
mastero101 <mastershift_101@hotmail.com>
2025-04-04 20:06:53 -0600

    refactor(environment): swap apiLoginUrl and apiLoginUrl2 values
    The values of apiLoginUrl and apiLoginUrl2 were swapped to align with the intended usage and improve clarity in the environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl2: 'http://localhost:3005/api',
   apiUrl: 'https://sistema-inventario-back.vercel.app/api',
-  apiLoginUrl2: 'https://demo-inventory-api.azurewebsites.net',
-  apiLoginUrl: 'http://132.145.206.61:8082'
+  apiLoginUrl: 'https://demo-inventory-api.azurewebsites.net',
+  apiLoginUrl2: 'http://132.145.206.61:8082'
 };
